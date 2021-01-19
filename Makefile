e1642ed0ec530c282e30a82cb030df22753332b8
Matt Kreul <mjkreul@iastate.edu>
2021-01-18 21:50:06 -0600

    Finalized project

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 all: FizzBuzz
 
 FizzBuzz: main.cpp
-	g++ -o FizzBuzz main.cpp std=c++11
+	g++ -o FizzBuzz main.cpp -std=c++11
 
 clean:
 	rm FizzBuzz
--- main.cpp
+++ main.cpp
@@ -6,53 +6,22 @@
  */
 
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
-//vector<int> fizzbuzz(){
-//	int numFound = 0;
-//	int iter = 1;
-//	vector<int> toRet;
-//	while (numFound < 100){
-//		if(iter%3 == 0){
-//			cout << "Fizz! (" << iter << "), ";
-//			toRet.push_back(iter);
-//			numFound++;
-//		}
-//		else if(iter%5 == 0){
-//			cout << "Buzz! (" << iter << "), ";
-//			toRet.push_back(iter);
-//			numFound++;
-//		}
-//		else{
-//			cout << iter << ", ";
-//		}
-//		if(iter%10 == 0){
-//			cout << endl;
-//		}
-//		iter++;
-//	}
-//	cout << endl;
-//	return toRet;
-//}
-
-
 int main() {
-//	vector<int> vecOfNum = fizzbuzz();
-	int numFound = 0;
+	int numFound = 0, numFizz = 0, numBuzz = 0;
 	int iter = 1;
-//	vector<int> toRet;
 	cout << "FizzBuzz Start!" << endl;
 	while (numFound < 100){
 		if(iter%3 == 0){
 			cout << "Fizz! (" << iter << "), ";
-//			toRet.push_back(iter);
+			numFizz++;
 			numFound++;
 		}
 		else if(iter%5 == 0){
 			cout << "Buzz! (" << iter << "), ";
-//			toRet.push_back(iter);
+			numBuzz++;
 			numFound++;
 		}
 		else{
@@ -64,6 +33,10 @@ int main() {
 		iter++;
 	}
 	cout << endl;
-	cout << "Thank you! Goodbye..." << endl;
+	cout << "Thank you! " << endl;
+	cout << "Totals: " << endl;
+	cout << "\tFizz: " << numFizz << endl;
+	cout << "\tBuzz: " << numBuzz << endl;
+	cout << "Goodbye..." << endl;
 	return 0;
 }
